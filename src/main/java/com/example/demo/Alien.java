b5482f89ccde54ccf90c7b0af4582c450e33a826
bfazeli <bfazeli1@gmail.com>
2019-10-14 16:24:43 -0700

    Initial Commit

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import lombok.Data;
+
+// If you want all the methods for Alien
+// Specify @Data in between public and class
+// Other wise use the annotations for what you want
+
+
+public @Data class Alien {
+    private int age;
+    private String name;
+    private String tech;
+}
+
+
